32bccdd1e1fdd48383b692731508af0a596452f6
acharchula <Matejki22>
2018-12-17 23:40:17 +0100

    Added connection with database

--- backend/src/main/java/pl/moviebook/BackendApplication.java
+++ backend/src/main/java/pl/moviebook/BackendApplication.java
@@ -1,5 +1,8 @@
 package pl.moviebook;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -7,15 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @Controller
 @SpringBootApplication
 public class BackendApplication {
 
-    @CrossOrigin(origins = "http://localhost:3000") //trzeba ustawic CORS globalnie
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "Dzień dobry - to początek backendu MovieBook'a!";
+    	
+			SessionFactory sessionFactory;
+			Configuration con = new Configuration().configure("hibernate.cfg.xml");
+			sessionFactory = con.buildSessionFactory();	
+			System.out.println("DB context created");
+			Session ses = sessionFactory.openSession();
+    	
+			int result = (int) ses.createNativeQuery("SELECT idCinema FROM  `Cinema`").getSingleResult();
+			
+		return Integer.toString(result);
 	}
 	
 	public static void main(String[] args) {
